7276a51baaea647097effd160be464c86f8d3b31
Manikanta <manikantachanda7@gmail.com>
2025-12-02 21:16:18 +0530

    Fix file casing for ToastProvider again

--- ui-event-app/src/pages/paymentPage.jsx
+++ ui-event-app/src/pages/paymentPage.jsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { useToast } from "../components/ToastProvider";
+import { useToast } from "../components/toastProvider";
 
 export default function PaymentPage() {
   const { state } = useLocation();
